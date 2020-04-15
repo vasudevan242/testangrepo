4478501db401485bd5606c4d1f7bcfd6fd6b489d
vasudevan242 <vasu.devan242@gmail.com>
2020-04-15 21:45:47 +0530

    fix in test

--- src/app/service/employeeservice.service.ts
+++ src/app/service/employeeservice.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-import { map } from 'rxjs/operators';
-
+import {environment} from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,23 +9,23 @@ export class EmployeeserviceService {
   constructor(private http:HttpClient) { }
 
   getEmployees() {
-    return this.http.get('http://dummy.restapiexample.com/api/v1/employees');
+    return this.http.get(environment.apiUrl+'employees');
   }
 
   createEmployee(postdata) {
-    return this.http.post('http://dummy.restapiexample.com/api/v1/create' , postdata);
+    return this.http.post(environment.apiUrl+'create' , postdata);
   }
 
   updateEmployee(id , postdata) {
-    return this.http.put(`http://dummy.restapiexample.com/api/v1/update/${id}` , postdata);
+    return this.http.put(`${environment.apiUrl}update/${id}` , postdata);
   }
 
   deleteEmployee(id:Number) {
-    return this.http.delete(`http://dummy.restapiexample.com/api/v1/delete/${id}`);
+    return this.http.delete(`${environment.apiUrl}delete/${id}`);
   }
 
   getEmployee(id:Number) {
-    return this.http.get(`http://dummy.restapiexample.com/api/v1/employee/${id}`);
+    return this.http.get(`${environment.apiUrl}employee/${id}`);
   }
 
 }
